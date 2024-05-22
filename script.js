ad59dbf02bddaa6ba854c432ab0309e492ce59ad
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-05-22 19:45:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	if (s === '') {
+        return '';
+    }
+
+    const spaceIndex = s.indexOf(' ');
+    if (spaceIndex === -1) {
+        return s;
+    }
+
+    return s.substring(0, spaceIndex);
+	
 }
 
 // Do not change the code below
